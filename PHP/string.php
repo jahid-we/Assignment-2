a7627768edbb6854ff9a1918601b9271cf3bd95d
Jahid Hasan <jahidhasan370919@gmail.com>
2024-05-18 20:42:27 +0600

    Method Changed

--- PHP/string.php
+++ PHP/string.php
@@ -1,9 +1,19 @@
 <?php
 $strings = ["Hello", "World", "PHP", "Programming"];
 
-foreach ($strings as $string) {
-    preg_match_all('/[a,e,i,o,u,A,E,I,O,U]/i', $string, $matches);
-    $vowelCount = count($matches[0]);
-    printf("Original String: %s, Vowel Count: %d, Reversed String: %s\n", $string, $vowelCount, strrev($string));
+foreach ($strings as $string){
+    $vowel = vowel($string);
+    printf("Original String: %s, Vowel Count: %d, Reversed String: %s\n", $string, $vowel, strrev($string));
 }
 
+function vowel($chr) {
+    $vowelArray = ["a", "e", "i", "o", "u"];
+    $chrLower = strtolower($chr);
+    $count = 0;
+    foreach (str_split($chrLower) as $chrSame) {
+        if (in_array($chrSame, $vowelArray)) {
+            $count++;
+        }
+    }
+    return $count;
+}
